e1ef0636c701386fdf5995ac554f897d0a8f49fc
Andrii Aheiev <a.ageyev@gmail.com>
2020-10-30 18:58:09 +0100

    dockerized version without auto ci

--- build-and-publish-docker-image.sh
+++ build-and-publish-docker-image.sh
@@ -1,22 +1,19 @@
 #!/bin/sh
 
-ECR_URL="821302506864.dkr.ecr.eu-west-1.amazonaws.com/notejam"
+ECR_URL="xxxxxxxxxxxx.dkr.ecr.eu-west-1.amazonaws.com/notejam"
 
 productVersion=$(cat VERSION)
 buildId=$(date +%s)
 assemblyVersion="${productVersion}.${buildId}"
-localImage="notejam:latest"
+localImage="notejam:${productVersion}"
 
 echo $productVersion
 echo $assemblyVersion
 
 # TODO: add arguments
-docker build -t ${localImage} .
-docker tag ${localImage} ${ECR_URL}:${assemblyVersion}
-docker push ${ECR_URL}:${assemblyVersion}
-
-# TODO: from master promote to ${productVersion}
-# TODO: PR to master promote to ${productVersion}-rc
-
+docker build \
+  --build-arg PRODUCT_VERSION=$productVersion \
+  --build-arg ASSEMBLY_VERSION=$assemblyVersion \
+  -t ${localImage} .
 docker tag ${localImage} ${ECR_URL}:${productVersion}
-docker push $ECR_URL:$productVersion
+docker push ${ECR_URL}:${productVersion}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,15 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.session_store :redis_session_store, {
+      key: ENV['APP_SESSION_KEY'],
+      redis: {
+          expire_after: 120.minutes,  # cookie expiration
+          ttl: 120.minutes,           # Redis expiration, defaults to 'expire_after'
+          key_prefix: 'notejam:session:',
+          url: ENV['REDIS_URL'],
+      }
+  }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -29,7 +38,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
